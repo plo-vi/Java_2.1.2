ca962a5348d6668a4dd0d4af9dd1517aedeeecd9
viktoriia287 <75584040+viktoriia287@users.noreply.github.com>
2021-01-23 21:41:04 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        double currentBalance = 100.55;
-        double payment = 1500.74;
+        int currentBalance = 100_55;
+        int payment = 1517_74;
 
-        double bonus;
+        int bonus;
         if (payment >= 1000) {
-            bonus = payment / 100;
+            bonus = payment / 100 / 100;
         } else {
             bonus = 0;
         }
 
-        /*System.out.printf("%.0f", bonus);*/
-
-        double newBalance = currentBalance + payment + bonus;
-        System.out.printf("%.2f",newBalance);
+        System.out.println(bonus);
     }
 }
